3b574a87d51517841f992ea344a7fd94055b0d25
pavan bhansali <pbhansali@localhost.localdomain>
2021-12-17 14:29:33 +0530

    props from child to parent

--- src/App.js
+++ src/App.js
@@ -1,18 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
 import Persons from "./containers/Persons";
+import React from 'react';
 
 //jsx -> javascript structured xml
 
-function App() {
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      textArray : []
+    }
+
+
+  }
+
+  onChangeHandler = (data)=> {
+console.log('App.js data---',data);
+
+// const dataArray = [...data];
+// console.log('data array---',dataArray);
+const dataArrayObj = [];
+for(let el of [...data]){
+  const obj = {
+    id: Date.now(),
+    value: el
+  };
+  dataArrayObj.push(obj)
+}
+
+console.log('datarrayobj---',dataArrayObj);
+
+
+
+this.setState({
+  textArray: dataArrayObj
+});
+
+  }
+
+  render(){
+
+  
+
   return (
     <div className="App">
      
-      <Persons salary = "$20000" designation = "Software Develeoper" />
+      <Persons change = {this.onChangeHandler} />
       {/* <Persons salary = "$50000" designation = "Program Director" ></Persons> */}
+
+      {
+        this.state.textArray.map((el) => {
+          return (
+            <div key={el.id} style = {{border: '2px solid cyan',borderRadius: '2px'}}>
+              <p>{el.value}</p>
+            </div>
+          )
+        })
+      }
 
     </div>
   );
+}
 }
 
 export default App;
--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -87,103 +87,24 @@ class Persons extends React.Component {
 
     }
 
+    onChangeHandler = (event) => {
+        console.log('event.target',event.target.value);
+        this.props.change(event.target.value);
+    }
+
 
     render() {
 
-        console.log('Person.js render called')
-        let disabled = (isNaN(Number(this.state.basicSalary)) || isNaN(Number(this.state.bonusSalary)));
-        if (this.state.basicSalary === '' || this.state.bonusSalary === '') {
-            disabled = true;
-        }
+    return(
+        <div>
+<label>Enter Text</label>
+<input onChange = {this.onChangeHandler} type = "text"/>
 
-        console.log('disabled', disabled);
-        const styleObj = {
-            border: "1px solid red"
-        }
+        </div>
 
-
-
-        return (
-            <div>
-                <div>
-                    <button onClick={this.togglePersonhandler} >Toggle Employee</button>
-                </div>
-                <h1>Employee Details</h1>
-
-
-                {
-                    this.state.showPersonInfo &&
-                    this.state.employees.map((el, index) => {
-                        return (
-                            <div>
-
-
-                                <div>
-                                    <p>Person Name: {el.name}</p>
-                                </div>
-                                <div style={styleObj}>
-                                    <div>
-                                        <label>Enter Basic Salary</label>
-                                        <input onChange={(event) => {
-                                            if (!isNaN(Number(event.target.value))) {
-                                                this.setState({
-                                                    basicSalary: event.target.value,
-                                                    basicSalaryerrorMessage: ''
-                                                });
-
-                                            } else {
-                                                this.setState({
-                                                    basicSalaryerrorMessage: "Please enter a numeric value"
-                                                })
-                                            }
-                                        }} type="text" />
-                                        {this.state.basicSalaryerrorMessage}
-
-
-                                    </div>
-                                    <div>
-                                        <label>Enter Bonus</label>
-                                        <input onChange={this.setBonusSalary} type="text" />
-                                        {this.state.bonusSalaryerrorMessage}
-
-                                    </div>
-
-                                    {/* <div>
-                                <label>Enter Salary</label>
-                                <input type="text"/>
-                            </div>
-                            <
-                             */}
-                                    <div>
-                                        <button disabled={disabled} onClick={this.onClickHandler}>Calculate</button>
-                                    </div>
-
-                                    <div Style={{ border: "1px solid cyan" }}>
-
-                                        <div>
-                                            Total Salary: {this.state.totalSalary}
-                                        </div>
-
-                                        <div>
-                                            PF: {this.state.basicSalary * 12 / 100}
-                                        </div>
-                                        <div>
-                                            Tax: {this.state.totalSalary * 5 / 100}
-                                        </div>
-
-                                    </div>
-
-                                </div>
-                            </div>
-                        )
-                    })
-
-                }
-
-
-
-            </div>
-        )
+    )
+     
+        
     }
 
 }
